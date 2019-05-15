6753db1cb3b576ede7d98b1b0d64122d52c60056
Junior.rodriguez <Junior.rodriguez@hectors-imac.hsd1.co.comcast.net>
2019-05-14 19:39:33 -0600

    I added the features to their corresponding buttons
    So yea, I added all of the features to their corresponding buttons, and
    also included Blake's drawing pad.

--- https/github.com/Lowkey310/PhotoShop-project.git/PhotoShop project/src/BlakeMethods.java
+++ https/github.com/Lowkey310/PhotoShop-project.git/PhotoShop project/src/BlakeMethods.java
@@ -13,12 +13,13 @@ public class BlakeMethods extends JPanel
 private final ArrayList<Point> point = new ArrayList<>();
 
 public BlakeMethods() {
-addMouseListener(new MouseAdapter() {
-public void mousePressed(MouseEvent event) {
-	point.add(event.getPoint());
-		            repaint();
-		        }
-		    });
+
+	addMouseListener(new MouseAdapter() {
+		public void mousePressed(MouseEvent event) {
+			point.add(event.getPoint());
+			repaint();
+			}
+	});
 
 		    addMouseMotionListener(new MouseMotionAdapter() {
 		        public void mouseDragged(MouseEvent event) {
@@ -31,12 +32,12 @@ public void mousePressed(MouseEvent event) {
 
 		public void paintComponent(Graphics g) {
 		    super.paintComponent(g);
-		    g.setColor(new Color(0, 0, 250));
+		    g.setColor(new Color(0, 0, 0));
 		    for (Point p : point)
 		        g.fillOval(p.x, p.y, 40, 40);
 		}
 
-		public static void main(String[] args) throws IOException {
+		public static void runner() throws IOException {
 			JFrame f = new JFrame();
 		    f.add(new BlakeMethods());
 		    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
